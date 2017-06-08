7742a76c29328f011c18deefd53e33bae2205db3
Atthar Mirza <atthar.mirza@gmail.com>
2017-06-07 23:46:42 -0400

    Project Covers Completed
    There is now a new UI for indexing all projects on the website.

--- javascript/indexGrid.js
+++ javascript/indexGrid.js
@@ -6,25 +6,46 @@ $(document).ready(function () {
         minWidth: 190,
     });
 
-    var scrollCount = 0;
-
     $('.Wrap').scroll(function (scrollCount) {
-        scrollCount + 1;
-        return scrollCount;
+        //add 1 if scrolled
+        scrollCount = 0;
+        scrollCount++;
+
+        //define additional boxes for DOM
+        var boxes = "<div class='box size12' id='motherMoon'><div class='projectTitle'><p>Mother Moon</p></div></div><div class='box size12' id='edovo'><div class='projectTitle'><p>Edovo Connect</p></div></div><div class='box size12' id='lowPoly'><div class='projectTitle'><p>Low Poly Animals</p></div></div><div class='box size21' id='projectED'><div class='projectTitle'><p>ProjectED Videos</p></div></div><div class='box size12' id='digitalArtist'><div class='projectTitle'><p>Digital Artist</p></div></div><div class='box size21' id='hotSauce'><div class='projectTitle'><p>Hot Sauce, White Sauce</p></div></div><div class='box size12' id='growing'><div class='projectTitle'><p>Growing in Pieces</p></div></div><div class='box size11' id='eyesAbove'><div class='projectTitle'><p>Eyes Above</p></div></div>";
+
+        //if you've scrolled, append the additional boxes
+        if (scrollCount == 1) {
+            $('.container').append(boxes).nested('append', boxes);
+            $(this).off('scroll');
+        };
+
+        highlightStart();
+        highlightStop();
     });
 
-    if (scrollCount >= 1) {
-        var scrollCount = true;
-        console.log('scrollCount = true')
-        return scrollCount;
-    } else {
-        console.log('scrollCount = false')
+    //jquery for styling follows
+    function highlightStart() {
+        $('.box').mouseenter(function () {
+            $(this).css('box-shadow', '0px 0px 0px 2px #ff6666 inset');
+            $(this).children().css({
+                'background-color': '#ff6666' /*,'box-shadow':'none'*/
+            });
+            $(this).find('p').css('color', '#ffffff');
+        });
     };
 
-    if (scrollCount) {
-        $('.container').append('<div class="box size11"></div><div class="box size12"></div><div class="box size21"></div><div class="box size12"></div><div class="box size12"></div> <div class = "box size12" > < /div><div class = "box size11" > < /div> <div class = "box size12" > < /div> <div class = "box size21" > < /div> <div class = "box size12" > < /div> <div class = "box size12" > < /div> <div class = "box size11" > < /div> <div class = "box size11" > < /div>');
-    } else {
-        console.log('Not Scrolled');
+    function highlightStop() {
+        $('.box').mouseleave(function () {
+            $(this).css('box-shadow', '0 0 15px #f2f2f2');
+            $(this).children().css({
+                'background-color': '#ffffff' /*,'box-shadow':'0px 0px 0px 2px #e6e6e6 inset'*/
+            });
+            $(this).find('p').css('color', '#000000');
+        });
     };
+
+    highlightStart();
+    highlightStop();
 
 });
